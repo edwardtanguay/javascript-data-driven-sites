659bd562b10b05479907ae3a246cfb401c6fc3e1
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-14 22:42:12 +0100

    new header

---
+++ fetchFromApi/js/main.js
@@ -0,0 +1,14 @@
+import { employees } from '../data/employees.js';
+
+const contentElem = document.querySelector('.content');
+
+contentElem.innerHTML += `There are ${employees.length} employees.`;
+
+contentElem.innerHTML += employees.map(emp => {
+	return `
+<h2>${emp.firstName} ${emp.lastName} (${emp.address.country})</h2>
+<section class="body">
+${emp.notes}
+</section>
+`;
+}).join('');
