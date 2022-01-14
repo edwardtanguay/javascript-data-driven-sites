66f62629b1e3f5749648985ac70a6b4f46c2b70e
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-14 22:55:38 +0100

    fetch finished

--- fetchFromApi/js/main.js
+++ fetchFromApi/js/main.js
@@ -1,14 +1,19 @@
-import { employees } from '../data/employees.js';
-
 const contentElem = document.querySelector('.content');
+contentElem.innerHTML = 'Loading...';
 
-contentElem.innerHTML += `There are ${employees.length} employees.`;
+(async () => {
+	const response = await fetch('https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json');
+	const employees = await response.json();
 
-contentElem.innerHTML += employees.map(emp => {
-	return `
+
+	contentElem.innerHTML = `There are ${employees.length} employees.`;
+
+	contentElem.innerHTML += employees.map(emp => {
+		return `
 <h2>${emp.firstName} ${emp.lastName} (${emp.address.country})</h2>
 <section class="body">
 ${emp.notes}
 </section>
 `;
-}).join('');
+	}).join('');
+})();
